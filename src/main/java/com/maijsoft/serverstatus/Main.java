74f1e067a439f40322399349cd6917cf84921de9
MAIJSOFT Dev <sean3226@naver.com>
2025-03-11 09:27:29 -0400

    Update Main.java

--- src/main/java/com/maijsoft/serverstatus/Main.java
+++ src/main/java/com/maijsoft/serverstatus/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 MAIJSOFT Dev
+ * Copyright (C) 2025 MAIJSOFT Dev
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
